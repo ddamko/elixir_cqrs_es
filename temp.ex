dac4e4a8edfb8e936535f8d17b6ff6d93341e5a6
Derek Damko <derek@toriangroup.com>
2015-03-27 10:18:08 -0700

    Clean up

--- temp.ex
+++
@@ -1,6 +0,0 @@
-{function_clause,
-  [{lists,reverse,
-    [[{'__struct__'=>'Elixir.Bank.Events.AccountCreated',date_created=>{{2015,3,24},{9,19,29}},id=>56}|nil]],
-    [{file,"lists.erl"},{line,145}]
-    },
-    {'Elixir.Bank.Account',loop,1,[{file,"lib/bank/account.ex"},{line,65}]}]}
